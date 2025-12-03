b3d4956e0cf7a164a7d66d59064fea95327a41d0
matheus-liber <101606042+matheus-liber@users.noreply.github.com>
2025-12-03 23:39:41 +0000

    `lint:check` e `lint:fix`

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
+
 Aprendizado de programação do Curso.dev
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,8 @@
 function Home() {
-    return <h1>Teste 2</h1>
+  return <h1>Teste 2</h1>;
 }
 
-export default Home; 
+export default Home;
+
+
+
