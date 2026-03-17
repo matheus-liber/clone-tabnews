ac2605258bfc1f384f83b73540bbbdaac0e8e702
matheus-liber <101606042+matheus-liber@users.noreply.github.com>
2026-03-17 17:27:34 +0000

    Criando log para database

--- infra/database.js
+++ infra/database.js
@@ -9,6 +9,15 @@ async function query(queryObject) {
     password: process.env.POSTGRES_PASSWORD,
     ssl:true,
   });
+  console.log('Credenciais do Postgres:', {
+    host: process.env.POSTGRES_HOST,
+    port: process.env.POSTGRES_PORT,
+    user: process.env.POSTGRES_USER,
+    database: process.env.POSTGRES_DB,
+    password: process.env.POSTGRES_PASSWORD,
+    ssl:true,
+    }
+  )
   
   try {
     await client.connect();
