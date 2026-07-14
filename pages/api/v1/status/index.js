dcfd462328e5c707dede0568c4b77d914f386bea
matheus-liber <101606042+matheus-liber@users.noreply.github.com>
2026-07-14 17:12:50 +0000

    fixes eslint

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,6 +1,6 @@
 import database from "infra/database.js";
 
-async function status(request, response) {
+async function status(request, response) {  
   const updatedAt = new Date().toISOString();
 
   const databaseVersionResult = await database.query("SHOW server_version;");
